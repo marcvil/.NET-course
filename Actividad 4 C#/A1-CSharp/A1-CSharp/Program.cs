44ca89b942d082a6375f317a20f0429c3acc59fc
marcvil <marcvilabascara@gmail.com>
2020-01-29 11:56:20 +0100

    C# Exercises

--- Actividad 4 C#/A1-CSharp/A1-CSharp/Program.cs
+++ Actividad 4 C#/A1-CSharp/A1-CSharp/Program.cs
@@ -4,9 +4,24 @@ namespace A1_CSharp
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Marc" ;
+            string surname1 = "Vilà";
+            string surname2 = "Espuna";
+
+            int day = 29;
+            int month = 01;
+            int year = 2020;
+
+            Console.WriteLine( surname1 + " " + surname2 + "," + name  );
+            Console.WriteLine(day + "/" + month + "/" + year);
+            Console.ReadKey();
+
         }
+
+
+
     }
 }
