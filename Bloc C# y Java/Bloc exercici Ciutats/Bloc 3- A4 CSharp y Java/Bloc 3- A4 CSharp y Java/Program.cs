14e9eff2f1b560cc324d7c79db2cbf71e1e8b377
marcvil <marcvilabascara@gmail.com>
2020-02-04 12:55:08 +0100

    04/02/2020

--- Bloc C# y Java/Bloc exercici Ciutats/Bloc 3- A4 CSharp y Java/Bloc 3- A4 CSharp y Java/Program.cs
+++ Bloc C# y Java/Bloc exercici Ciutats/Bloc 3- A4 CSharp y Java/Bloc 3- A4 CSharp y Java/Program.cs
@@ -14,45 +14,49 @@ namespace Bloc_3__A4_CSharp_y_Java
             string city5;
             string city6;
 
+            ///Array string de city
             string[] cityArray = new string[6];
+
+
+            
 
             //Read USerInput
             for (int i = 0; i < 6; i++)
             {
                 Console.Write("Escriu el nom de la Ciutat " + (i + 1) + "\n");
                 cityArray[i] = Console.ReadLine();
+                
+
+                
             }
             Console.Write("Enter per escriure el nom de les ciutats. \n");
             Console.ReadKey();
-            
 
-            for (int j = 0; j < 6; j++)
+            //Arrays para conseguir el reverso
+            string[] city1lettersArray = new string[city1.Length];
+
+
+            string[] city2lettersArray = new string[city2.Length];
+
+
+            string[] city3lettersArray = new string[city3.Length];
+
+
+            string[] city4lettersArray = new string[city4.Length];
+
+
+            string[] city5lettersArray = new string[city5.Length];
+
+
+            string[] city6lettersArray = new string[city6.Length];
+
+
+            for (int i = cityArray[0].Length - 1; i >= 0; i--)
             {
-                string[] currentCityArray = new string[cityArray[j].Length];
-                
-                for (int k = 0; k < currentCityArray.Length; k++)
-                {
-                    char[] temp = new char[currentCityArray.Length];
-                    temp = cityArray[j].ToCharArray();
-                    
-                    
-                    for (int m = 0; m <=temp.Length; m++ )
-                    {
-                        currentCityArray[m] = temp[k].ToString();
-                        Console.Write(currentCityArray[m] + "\n");
-                    }
-                    
-                    
-                    
-                }
-                
-
-                /*
-                cityArray[j] = cityArray[j].Replace("a", "4");
-
-                Console.Write("El nom de la ciutat " + (j + 1) + " és: " + cityArray[j] + "\n");
-                */
+                city1lettersArray[i] = cityArray[0].Substring(i, 1);
             }
+
+
         }
     }
 }
--- Bloc C# y Java/Bloc exercici lletres/Bloc 2 - Actividad 3/Bloc 2 - Actividad 3/Program.cs
+++ Bloc C# y Java/Bloc exercici lletres/Bloc 2 - Actividad 3/Bloc 2 - Actividad 3/Program.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace Bloc_2___Actividad_3
 {
+    class Letter
+    {
+        public string letter;
+        public int numberRepetitions;
+    }
+
+
+
     class Program
     {
+      
         static void Main(string[] args)
         {
             Console.WriteLine("This application will split your name by it's letters.");
@@ -36,13 +45,44 @@ namespace Bloc_2___Actividad_3
 
         private static void ManagingLetters(IDictionary<int, string> dictionaryletters)
         {
-            //for loop para comparar letras en la lista con array de vocales,  NO TIENE EN CUENTA ACENTOS!!!!!
+            foreach (string c in dictionaryletters.Values)
+            {
+                int numberOfRepetitions = 0;
+                for (int l = 0; l < dictionaryletters.Count; l++)
+                {
+                    if (c == dictionaryletters[l] && l < (dictionaryletters.Count - 1))
+                    {
+                        numberOfRepetitions = numberOfRepetitions + 1;
+
+                    }
+                    if (c == dictionaryletters[l] && l == (dictionaryletters.Count - 1))
+                    {
+                        numberOfRepetitions = numberOfRepetitions + 1;
+                        
+                        
+                    }
+                    if (c != dictionaryletters[l] && l == (dictionaryletters.Count - 1))
+                    {
+                        
+
+                    }
+                    
+
+                }
+                Console.Write("El numero de repeticiones  de la letra:" + c + " es " + numberOfRepetitions + "\n");
+                
+
+            }
+
+            /*
+              //for loop para comparar letras en la lista con array de vocales,  NO TIENE EN CUENTA ACENTOS!!!!!
             string[] vocals = new string[5];
             vocals[0] = "a";
             vocals[1] = "e";
             vocals[2] = "i";
             vocals[3] = "o";
             vocals[4] = "u";
+
 
             for (int j = 0; j < dictionaryletters.Count; j++)
             {
@@ -64,8 +104,43 @@ namespace Bloc_2___Actividad_3
                         break;
                     }
                 }
-            }
+                */
+
+
+
+
+
+
         }
     }
-}
+    }
+}/*         /*
+                if (lettersRepeated.Count == 0)
+                {
+                    Letter letter = new Letter();
+                    letter.letter = dictionaryletters[j].ToString();
+                    letter.numberRepetitions++;
+                    lettersRepeated.Add(letter);
+                    
+                }
+                for (int l = 0; l < lettersRepeated.Count; l++)
+                {
+                   
+                    if (dictionaryletters[j] != lettersRepeated[l].letter && l == (lettersRepeated.Count))
+                    {
+                        Letter letter = new Letter();
+                        letter.letter = dictionaryletters[j];
+                        letter.numberRepetitions++;
+                        lettersRepeated.Add(letter);
+                        Console.Write(lettersRepeated[l].letter, lettersRepeated[l].numberRepetitions);
+                        continue;
+                    }
+                    if (dictionaryletters[j] == lettersRepeated[l].letter)
+                    {
+                        lettersRepeated[l].numberRepetitions++;
+                        Console.Write(lettersRepeated[l]);
+                    }
+                    //Console.Write(lettersRepeated[l].letter, lettersRepeated[l].numberRepetitions);
+                }
+               */
 
