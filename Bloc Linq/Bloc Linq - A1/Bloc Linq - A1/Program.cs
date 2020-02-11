f1da054fe48aec5965b4478ec093fb8fa52a16e0
marcvil <marcvilabascara@gmail.com>
2020-02-11 13:14:38 +0100

    Empezar Linq y A1 practica troncal C#

---
+++ Bloc Linq/Bloc Linq - A1/Bloc Linq - A1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Bloc_Linq___A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[15] { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+
+            
+            
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
