113db100400b31dbf14e4a124c66d9c67f7bc457
marcvil <marcvilabascara@gmail.com>
2020-02-12 13:03:25 +0100

    A2 practica troncal

---
+++ Bloc C# y Java/Practica Troncal/Blo 5- Práctica Troncal A2/Blo 5- Práctica Troncal A2/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Blo_5__Práctica_Troncal_A2
+{
+
+
+    public class Persona
+    {
+
+        string name { get; set; }
+        int age { get; set; }
+        int dni { get; set; }
+
+
+
+        public Persona()
+        {
+
+        }
+        public Persona(string name, int age, int dni)
+        {
+
+            this.name = name;
+            this.age = age;
+            this.dni = dni;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("método: la persona con nombre " + name
+                    + " que tiene " + age + " años"
+                    + " y que tiene el siguiente dni: " + dni);
+        }
+
+
+    }
+
+    
+
+}
---
+++ Bloc C# y Java/Practica Troncal/Blo 5- Práctica Troncal A2/Blo 5- Práctica Troncal A2/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Blo_5__Práctica_Troncal_A2
+{
+
+    public class Subject
+    {
+        int idSubject { get; set; }
+        string subjectName { get; set; }
+
+        public Subject()
+        {
+
+        }
+        public Subject( int idSubject, string subjectName)
+        {
+            this.idSubject = idSubject;
+            this.subjectName = subjectName;
+        }
+    }
+
+}
