cb7b176a2d356dddf1a596742309e7ad18ed0d50
marcvil <marcvilabascara@gmail.com>
2020-02-12 12:09:01 +0100

    Linq exercise completed

--- Bloc Linq/Bloc Linq - A1/Bloc Linq - A1/Program.cs
+++ Bloc Linq/Bloc Linq - A1/Bloc Linq - A1/Program.cs
@@ -10,11 +10,14 @@ namespace Bloc_Linq___A1
         {
             int[] numArray = new int[15] { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
 
+            var numQuery = from num in numArray
+                             where (num % 2) == 0
+                             select num;
             
-            
-            
-            
-            Console.WriteLine("Hello World!");
+            foreach(int num in numQuery)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
